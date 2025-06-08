9e7dd965a90192dff1c06925d779ef9655ad70de
shubham876623 <shubhamsaini8766@gmail.com>
2025-06-08 16:32:08 +0530

    fixed the loged in issue

--- src/api/axiosWithRefresh.js
+++ src/api/axiosWithRefresh.js
@@ -5,6 +5,7 @@ const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL,
+  withCredentials: true       
   // headers: {
   //   "Content-Type": "application/json",
   // },
